c99bfedd7bbaa4c2a7c6c9ac421773f88c0ebdb6
Jose Edson Jr <edsonjunior9701@outlook.com>
2026-04-09 10:00:43 -0300

    configuracao do create de lugares e consumo de api

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-lugar',
@@ -8,12 +9,12 @@ import { Categoria } from '../../categorias/categoria';
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss'
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit{
 
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(){
+  constructor(private categoriaService: CategoriaService){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -21,6 +22,16 @@ export class LugarComponent {
       urlFoto: new FormControl('', Validators.required),
       avaliacao: new FormControl('', Validators.required),
     });
+  }
+
+  ngOnInit(): void {
+    //console.log('Carregando as categorias da API');
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => {
+      //console.log('rebendo categorias: ', listaCategorias);
+      this.categorias = listaCategorias
+      }
+    })
   }
 
   salvar(){
